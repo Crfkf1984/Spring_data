80305347bbb708712c70e3bf03cc2359d38a1a3d
Crfkf1984! <seirosso1@gmail.com>
2022-01-17 17:52:19 +0300

    Spring Boot data ReST

---
+++ src/main/java/com/example/spring_data/dao/TodoRepository.java
@@ -0,0 +1,13 @@
+package com.example.spring_data.dao;
+
+
+
+import com.example.spring_data.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
+    List<Todo> findAllByTitle(String title);
+}
